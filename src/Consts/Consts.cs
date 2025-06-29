174d00ea5805522e2aa2c0676af957ca0a97cefb
mcahyadit <60179302+mcahyadit@users.noreply.github.com>
2025-06-29 21:27:00 +0800

    v1.0.1.0
    - changed char Consts to use unicode instead

--- src/Consts/Consts.cs
+++ src/Consts/Consts.cs
@@ -2,8 +2,8 @@ namespace MMOR.Utils.Consts
 {
     public static class Consts
     {
-        public const char multiplierSign = '�';
-        
-        public const char plusMinusSign = '�';
+        public const char multiplierSign = '\u00D7';
+
+        public const char plusMinusSign = '\u00B1';
     }
 }
