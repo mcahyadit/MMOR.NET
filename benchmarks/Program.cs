7d254563c0c9a26d30b4db6ce633b0006aeea676
mcahyadit <mcahyadit@github.com>
2026-08-03 22:42:17 +0800

    build(Benchmark): added global.json to anchor the root so that .direnv doesn't mess around with the generated projects

--- benchmarks/Program.cs
+++ benchmarks/Program.cs
@@ -1,9 +1,18 @@
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 
 namespace MMOR.NET.Benchmarks {
 public static class Program {
   public static void Main(string[] args) {
-    BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    ManualConfig config = DefaultConfig
+                              .Instance  //
+                              .AddJob(Job.Default.WithRuntime(CoreRuntime.Core10_0))
+                              .AddJob(Job.Default.WithRuntime(NativeAotRuntime.Net10_0))
+                              .AddJob(Job.Default.WithRuntime(CoreRuntime.Core80))
+                              .AddJob(Job.Default.WithRuntime(NativeAotRuntime.Net80));
+    BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
   }
 }
 }
